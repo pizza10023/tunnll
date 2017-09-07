3884a77b6c0166c8a2d1d55df887bdb87a5bc7ad
Terry Liu <liuterryliu@gmail.com>
2017-09-06 21:44:07 -0500

    Commiting all files in project folder, also using widgets without findByViewId. Note currently using Android Studio 3.0

--- MyFirstKotlinApp/app/src/main/kotlin/com/example/terry/myfirstkotlinapp/MainActivity.kt
+++ MyFirstKotlinApp/app/src/main/kotlin/com/example/terry/myfirstkotlinapp/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.terry.myfirstkotlinapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        textView1.setText("Changed without findViewById")
+        textView2.setText("Also changed with just one import statement")
     }
 }
