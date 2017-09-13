0f5ad3175af1399db971ad6316d694d7e2a5d93d
Terry Liu <liuterryliu@gmail.com>
2017-09-12 23:16:55 -0500

    Can see user location and compass.

--- KotlinOpenStreetMap/app/src/main/java/com/example/terry/kotlinopenstreetmap/MainActivity.kt
+++ KotlinOpenStreetMap/app/src/main/java/com/example/terry/kotlinopenstreetmap/MainActivity.kt
@@ -20,20 +20,36 @@ import org.osmdroid.util.GeoPoint
 import org.osmdroid.api.IMapController
 import android.Manifest.permission
 import android.Manifest.permission.WRITE_CALENDAR
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
 import android.support.v4.content.ContextCompat
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
-
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.compass.CompassOverlay
 
 class MainActivity : AppCompatActivity() {
 
+    private val MAP_DEFAULT_LATITUDE = 44.445883
+    private val MAP_DEFAULT_LONGITUDE = 26.040963
+
     private val map:MapView by lazy {
-        map_l.setBuiltInZoomControls(true);
-        map_l.setMultiTouchControls(true);
+        map_l.setBuiltInZoomControls(true)
+        map_l.setMultiTouchControls(true)
+        map_l.setTileSource(TileSourceFactory.MAPNIK)
+        map_l.setClickable(true)
         map_l
 
     }
 
+
+    private var mItemizedOverlay: ItemizedOverlay<OverlayItem>?=null
+    private var lastPosition: OverlayItem? = null
+    private var mLocationOverlay:MyLocationNewOverlay ?=null
+    private var compassOverlay:CompassOverlay?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val ctx = applicationContext
@@ -49,19 +65,40 @@ class MainActivity : AppCompatActivity() {
         val coarseLocationCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION)
         setSupportActionBar(toolbar)
-        map.setTileSource(TileSourceFactory.MAPNIK)
-        var mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(ctx), map)
-        mLocationOverlay.enableMyLocation()
-        map.getOverlays().add(mLocationOverlay)
         val mapController = map.controller
-        mapController.setZoom(9)
+        mapController.setZoom(14)
         val startPoint = GeoPoint(40.1106, -88.2073)
         mapController.setCenter(startPoint)
+        mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(ctx), map)
+        mLocationOverlay?.enableMyLocation()
+        mLocationOverlay?.enableFollowLocation()
+        mLocationOverlay?.runOnFirstFix(Runnable { mapController.animateTo(mLocationOverlay?.getMyLocation()) })
+        map.getOverlays().add(mLocationOverlay)
+        compassOverlay = CompassOverlay(this, map)
+        compassOverlay?.enableCompass()
+        map.getOverlays().add(compassOverlay)
+
+
+
+        val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        var location:Location?=null
+
+
     }
 
     override fun onResume() {
         super.onResume()
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        mLocationOverlay?.enableMyLocation()
+        mLocationOverlay?.enableFollowLocation()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mLocationOverlay?.disableMyLocation()
+        mLocationOverlay?.disableFollowLocation()
     }
 
 
@@ -85,4 +122,5 @@ class MainActivity : AppCompatActivity() {
         } else super.onOptionsItemSelected(item)
 
     }
+
 }
